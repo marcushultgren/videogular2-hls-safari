d2fa504f540cc9bedc5740dc0a1997ec7b9e6577
Marcus Hultgren <marcus@brilliantworks.se>
2018-11-16 12:15:01 +0100

    Add videogular hls example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { VgHLS } from 'videogular2/src/streaming/vg-hls/vg-hls';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild(VgHLS) vgHls: VgHLS;
   title = 'videogular-hls';
+  currentStream = 'https://content.jwplatform.com/manifests/yp34SRmf.m3u8';
+
+  ngOnInit() {
+  }
+
+  onPlayerReady(api) {
+    debugger;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {VgCoreModule} from 'videogular2/core';
+import {VgControlsModule} from 'videogular2/controls';
+import {VgOverlayPlayModule} from 'videogular2/overlay-play';
+import {VgBufferingModule} from 'videogular2/buffering';
+import {VgStreamingModule} from 'videogular2/streaming';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule,
+    VgStreamingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
